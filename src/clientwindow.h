c17e64c69e950367e25902b6fa34748c9a444198
xvlaci <fera@ubuntu.ubuntu-domain>
2014-05-23 14:00:09 +0200

    Smazany prebytecne metody

--- src/clientwindow.h
+++ src/clientwindow.h
@@ -18,12 +18,6 @@
 #include <QGraphicsScene>
 #include <QMessageBox>
 
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
-#include <pthread.h>
-#include <time.h>
-#include <boost/date_time/posix_time/posix_time.hpp>
-
 #include "client.h"
 
 namespace Ui {
@@ -37,10 +31,6 @@ class ClientWindow : public QWidget
 public:
     explicit ClientWindow(QWidget *parent = 0);
     ~ClientWindow();
-    ClientWindow * me;
-    ClientWindow * getMe(){
-        return this->me;
-    }
 
 private:
     Ui::ClientWindow *ui;
@@ -53,29 +43,6 @@ private:
 
     void repaint(std::string board_state);
     void start_connection();
-    pthread_t thread;
-    void * repainter(void *threadid){
-        //while(true){
-          //  sleep(1);
-
-         //   ClientWindow * he = ClientWindow::getMe();
-
-           // client * cl = he->my_client;
-            //std::string s = cl->getState();
-            //std::cout << "Takze to fakt pada tady" << std::endl;
-            //if(this->my_client->state_[0] != 'q'){
-              //  std::cout << "Este jedna moznost" << std::endl;
-            //}
-                //repaint(this->my_client->state_);
-            //std::cout << "Nebo taky ne?" << std::endl;
-        //}
-        pthread_exit(NULL);
-    }
-
-    static void * JHWrapper(void *self){
-       ClientWindow *that = static_cast<ClientWindow*>(self);
-       return that->repainter(self);
-    }
 
 
 
